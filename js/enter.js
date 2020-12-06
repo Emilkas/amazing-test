32762988892de92a68b0733c8f5a3e40019b50f7
Vanek <30772009+Emilkas@users.noreply.github.com>
2020-12-07 01:07:18 +0700

    Delete enter.js

--- js/enter.js
+++
@@ -1,5 +0,0 @@
-function Reg() {  
-    document.getElementById("Reg").setAttribute("style", "display: none");  
-    document.getElementById("s-in").setAttribute("style", "display: none");          
-    document.getElementById("s-up").setAttribute("style", "display: block");        
-}
